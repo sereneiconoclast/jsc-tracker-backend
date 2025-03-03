9d9ad91a1305295cf913e95727179149424e5f0e
Greg Meyers <greg.meyers.1138@gmail.com>
2025-03-02 16:29:11 -0800

    Don't update created_at on every write
    We have modified_at for that purpose.

--- lib/db.rb
+++ lib/db.rb
@@ -1,48 +1,47 @@
 require 'active_support/core_ext/hash/keys' # symbolize_keys
 require 'aws-sdk-dynamodb'
 
-  class DB
-    DEBUG = false
+class DB
+  DEBUG = true
 
-    def initialize(table:, region:)
-      @table_name = table
-      @region = region
-      # In theory we could pass access_key_id: and secret_access_key:
-      # For now, we get those from ~/.aws/credentials
-      @dynamodb = Aws::DynamoDB::Client.new(region: region)
-    end
-
-    attr_reader :dynamodb, :table_name, :region
-
-    def read pk:
-      dynamodb.get_item(
-        {
-          table_name: table_name,
-          key: { pk: pk }
-        }
-      )&.item.tap do |item|
-        puts("DynamoDB read #{pk} => #{item ? JSON.pretty_generate(item) : 'Not found'}") if DEBUG
-      end&.symbolize_keys
-    end
-
-    def write item:
-      item = item.merge(created_at: Time.now.to_i)
-      puts "DynamoDB write #{JSON.pretty_generate(item)}" if DEBUG
-      raise "Oops: no pk" unless item[:pk]
-      dynamodb.put_item(
-        table_name: table_name,
-        item: item
-      )
-    rescue Aws::DynamoDB::Errors::ServiceError => error
-      warn "Unable to update DynamoDB: #{error}"
-      raise
-    end
-
-    def delete pk:
-      dynamodb.delete_item({ table_name: table_name, key: { pk: pk }})
-      puts "DynamoDB delete #{pk}"
-    rescue Aws::DynamoDB::Errors::ServiceError => error
-      warn "Unable to update DynamoDB: #{error}"
-      raise
-    end
+  def initialize(table:, region:)
+    @table_name = table
+    @region = region
+    # In theory we could pass access_key_id: and secret_access_key:
+    # For now, we get those from ~/.aws/credentials
+    @dynamodb = Aws::DynamoDB::Client.new(region: region)
   end
+
+  attr_reader :dynamodb, :table_name, :region
+
+  def read pk:
+    dynamodb.get_item(
+      {
+        table_name: table_name,
+        key: { pk: pk }
+      }
+    )&.item.tap do |item|
+      puts("DynamoDB read #{pk} => #{item ? JSON.pretty_generate(item) : 'Not found'}") if DEBUG
+    end&.symbolize_keys
+  end
+
+  def write item:
+    puts "DynamoDB write #{JSON.pretty_generate(item)}" if DEBUG
+    raise "Oops: no pk" unless item[:pk]
+    dynamodb.put_item(
+      table_name: table_name,
+      item: item
+    )
+  rescue Aws::DynamoDB::Errors::ServiceError => error
+    warn "Unable to update DynamoDB: #{error}"
+    raise
+  end
+
+  def delete pk:
+    dynamodb.delete_item({ table_name: table_name, key: { pk: pk }})
+    puts "DynamoDB delete #{pk}"
+  rescue Aws::DynamoDB::Errors::ServiceError => error
+    warn "Unable to update DynamoDB: #{error}"
+    raise
+  end
+end
