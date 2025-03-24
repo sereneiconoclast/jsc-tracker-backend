ee632437bfee53965a9c7b1fdd3a5a6210660448
Greg Meyers <greg.meyers.1138@gmail.com>
2025-03-23 18:49:35 -0700

    Rename 'GetUserInfo' to 'GetUserUserId'
    The name now mirrors the HTTP verb and path, which are GET /user/{user_id}
    
    Rolling these changes out to CloudFormation had to be done in several steps:
    
    * Create a copy of the handler file
    * Create a duplicate method and a new exported output in the lambda stack
    * Remove the path /user/{user_id} and everything depending on it from the API gateway stack
    * Re-add the path and the new method, referring to the new-named Lambda method
    * Remove the now-unreferenced resources from the lambda stack
    * Remove the now-unused handler file

--- lib/get_user_user_id.rb
+++ lib/get_user_user_id.rb
@@ -1,5 +1,6 @@
 require_relative 'jsc/user'
 
+# GET /user/{user_id}
 def lambda_handler(event:, context:)
   user = Jsc::User.read(email: event.dig('pathParameters', 'user_id'))
   users = [user&.to_json_hash].compact
