66c8ab89bb268300c6696095f574a87944be64a6
Greg Meyers <greg.meyers.1138@gmail.com>
2025-04-20 09:27:18 -0700

    Revise get_user_user_id.rb handler to use standard_json_handling
    This monkey-patched method now handles generating the JSON and specifying the HTTP response code

--- lib/get_user_user_id.rb
+++ lib/get_user_user_id.rb
@@ -2,24 +2,13 @@ require_relative 'jsc/require_all'
 
 # GET /user/{user_id}
 def lambda_handler(event:, context:)
-  user = Jsc::User.read(email: event.dig('pathParameters', 'user_id'))
-  users = [user&.to_json_hash].compact
-  {
-    statusCode: 200,
-    body: JSON.generate({
+  standard_json_handling(event: event) do |body|
+    user = Jsc::User.read(email: event.dig('pathParameters', 'user_id'))
+    users = [user&.to_json_hash].compact
+    {
       users: users,
       jsc: nil,
       jsc_members: nil
-    })
-  }
-rescue DynamoObject::NotFoundError => e
-  {
-    statusCode: 404,
-    body: JSON.generate({ error: e.message })
-  }
-rescue StandardError => e
-  {
-    statusCode: 500,
-    body: JSON.generate({ error: e.message })
-  }
+    }
+  end
 end # lambda_handler
