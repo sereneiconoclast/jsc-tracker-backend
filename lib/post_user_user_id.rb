69118def76675b5a638ba6d50c337d3a6e4d440b
Greg Meyers <greg.meyers.1138@gmail.com>
2025-04-20 09:29:22 -0700

    WIP (unfinished): Handle post to user
    Employ standard_json_handling as the corresponding GET call does.
    
    This should use Jsc::User::ALLOWED_IN_USER_USER_ID_POST to constrain what fields are allowed to be written to the User. If any other keys are sent in the POST then they are filtered out.

--- lib/post_user_user_id.rb
+++ lib/post_user_user_id.rb
@@ -1,23 +1,19 @@
 require_relative 'jsc/require_all'
+require 'json'
 
 # POST /user/{user_id}
 def lambda_handler(event:, context:)
-  # user = Jsc::User.read(email: event.dig('pathParameters', 'user_id'))
-  {
-    statusCode: 200,
-    body: JSON.generate({
-      event: event,
-      context_class: context.class.instance_methods(false)
-    })
-  }
-rescue DynamoObject::NotFoundError => e
-  {
-    statusCode: 404,
-    body: JSON.generate({ error: e.message })
-  }
-rescue StandardError => e
-  {
-    statusCode: 500,
-    body: JSON.generate({ error: e.message })
-  }
+  standard_json_handling(event: event) do |body|
+    user = Jsc::User.read(email: event.dig('pathParameters', 'user_id'))
+
+    body.keep_if do |k, _v|
+      ALLOWED_IN_USER_USER_ID_POST.include?(k)
+    end
+
+    # TODO: Write these fields into the user, then
+    # update DynamoDB
+
+    # Return the portion we accepted
+    body
+  end
 end # lambda_handler
