33295151e02636c3e57d1f87b9c37632f3f1341b
Greg Meyers <greg.meyers.1138@gmail.com>
2025-04-20 09:22:45 -0700

    Remove 'db' and 'jsc_email_sha1_salt' from application_config.rb
    These are monkey-patches and now live together with the others
    
    Also define JSC_TRACKER_FQDN

--- lib/application_config.rb
+++ lib/application_config.rb
@@ -1,42 +1,5 @@
-require 'aws-sdk-secretsmanager'
-require_relative 'monkey_patches/require_all'
-
-require 'db'
-
 JSC_REGION = 'us-west-2'
 
-# Very important! CloudFront must be configured with behavior:
-# * Redirect HTTP to HTTPS
-# * Allow all HTTP methods
-# * Cache policy: CachingDisabled (otherwise requests will go through with a stale 'google_state')
-# * Origin request policy: AllViewer (otherwise query string parameters will not pass through)
-# * Turn OFF AWS WAF, or it will block requests from some devices - the Echo Show devices -
-#   because the requests are too large and WAF blocks them
+JSC_TRACKER_FQDN = 'jsc-tracker.infinitequack.net' # API Gateway alias 'A' record
 
-CNAME_FQDN = 'jsc.infinitequack.net' # CloudFront CNAME 'abcdefghijklmn.cloudfront.net'
-
-# Use this code snippet in your app.
-# If you need more information about configurations or implementing the sample code, visit the AWS docs:
-# https://aws.amazon.com/developer/language/ruby/
-
-module ::Kernel
-  def jsc_email_sha1_salt
-    $jsc_email_sha1_salt ||= begin
-      $secrets_manager_client ||= Aws::SecretsManager::Client.new(region: JSC_REGION)
-
-      # See jsc-tracker-lambda.yaml for the secret ID
-      # For a list of exceptions thrown, see
-      # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-      get_secret_value_response = $secrets_manager_client.get_secret_value(secret_id: 'jsc-tracker-email-sha1-salt')
-
-      JSON.parse(get_secret_value_response.secret_string)['email_sha1_salt']
-    end
-  end
-
-  def db
-    $db ||= begin
-      DB.new(table: 'JSC-Tracker', region: JSC_REGION)
-    end
-  end
-end
-
+require_relative 'monkey_patches/require_all'
