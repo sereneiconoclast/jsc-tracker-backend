be5d1bb207279d3d5d98b244ed5283fb8f2aa9b6
Greg Meyers <greg.meyers.1138@gmail.com>
2025-05-10 16:00:32 -0700

    Properly support CORS with two possible Origins
    Access-Control-Allow-Origin must be set in the response headers to match the caller's own Origin header in the request. This is, to me, a bit of an odd architecture. The server could just as well return a boolean for 'yes, fine' or 'no, not fine', but instead it returns the value back if it's acceptable. Anyway, it seems to make the browser happy.

--- lib/monkey_patches/standard_json_handling.rb
+++ lib/monkey_patches/standard_json_handling.rb
@@ -2,32 +2,46 @@ require 'json'
 require 'dynamo_object'
 
 module ::Kernel
+  JSC_CORS_ALLOWED_ORIGINS = %w(
+    https://static.infinitequack.net
+    http://localhost:3000
+  )
   JSC_CORS_HEADERS = {
-    "Access-Control-Allow-Origin" => "'https://static.infinitequack.net,http://localhost:3000'",
+    "Access-Control-Allow-Origin" => nil,
     "Access-Control-Allow-Headers" => "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'",
     "Access-Control-Allow-Methods" => "'GET,POST,PUT,DELETE,OPTIONS'",
     "Access-Control-Max-Age" => "'600'",
   }
 
   def standard_json_handling(event:)
-    body = JSON.parse(event['body'])
+    origin = event.dig('headers', 'origin')
+    unless JSC_CORS_ALLOWED_ORIGINS.include?(origin)
+      return({
+        statusCode: 404,
+        body: JSON.generate({ error: "Not found" })
+      })
+    end
+    response_headers = JSC_CORS_HEADERS.dup
+    response_headers['Access-Control-Allow-Origin'] = origin
+
+    body = JSON.parse(event['body'] || '{}')
     result_hash = yield(body: body)
     {
       statusCode: 200,
-      headers: JSC_CORS_HEADERS,
+      headers: response_headers,
       body: JSON.generate(result_hash)
     }
   rescue DynamoObject::NotFoundError => e
     {
       statusCode: 404,
-      headers: JSC_CORS_HEADERS,
-      body: JSON.generate({ error: e.message })
+      headers: response_headers,
+      body: JSON.generate({ error: e.message, backtrace: e.backtrace })
     }
   rescue JSON::ParserError, StandardError => e
     {
       statusCode: 500,
-      headers: JSC_CORS_HEADERS,
-      body: JSON.generate({ error: e.message })
+      headers: response_headers,
+      body: JSON.generate({ error: e.message, backtrace: e.backtrace })
     }
   end
 end
