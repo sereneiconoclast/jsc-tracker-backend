cab7f633cff20fa42379845a9cb6d9c8689aa927
Greg Meyers <greg.meyers.1138@gmail.com>
2025-05-24 07:03:25 -0700

    get_user_user_id: Accept a dash instead of the actual ID
    Common use case: The user wishes to view their own user information, and generally, the common information they are shown: information on their fellow JSC members. Passing a hyphen instead of the ID means, use the value of 'sub' from the Google Access Token as the user ID. If the user isn't found in DDB, create it immediately, using the values from the token.

--- lib/get_user_user_id.rb
+++ lib/get_user_user_id.rb
@@ -2,9 +2,24 @@ require_relative 'jsc/require_all'
 
 # GET /user/{user_id}
 def lambda_handler(event:, context:)
-  standard_json_handling(event: event) do |body|
-    user = Jsc::User.read(sub: event.dig('pathParameters', 'user_id'))
-    users = [user&.to_json_hash].compact
+  standard_json_handling(event: event) do |body:, access_token:|
+
+    user_id = event.dig('pathParameters',  'user_id')
+
+    if user_id == '-'
+      user_id = access_token['sub']
+      user = Jsc::User.read(sub: user_id, ok_if_missing: true)
+      unless user
+        args = access_token.keep_if do |k, _v|
+          Jsc::User::DEFAULTED_FROM_ACCESS_TOKEN.include?(k)
+        end
+        user = Jsc::User.new(**args)
+        user.write!
+      end
+    else
+      user = Jsc::User.read(sub: user_id)
+    end
+    users = [user.to_json_hash].compact
     {
       users: users,
       jsc: nil,
