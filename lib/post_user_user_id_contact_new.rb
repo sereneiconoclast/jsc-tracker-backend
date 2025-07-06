04d991eccca492f5bbf680fc03490d18e9925051
Greg Meyers <greg.meyers.1138@gmail.com>
2025-07-05 19:39:19 -0700

    Add POST API: /user/<user_id>/contact/new

---
+++ lib/post_user_user_id_contact_new.rb
@@ -0,0 +1,17 @@
+require_relative 'jsc/require_all'
+require 'json'
+
+# POST /user/{user_id}/contact/new
+def lambda_handler(event:, context:)
+  standard_json_handling(event: event) do |body:, access_token:|
+    user = Jsc::User.read(sub: event.dig('pathParameters', 'user_id'))
+
+    # Create new contact using the user's add_contact method
+    contact = user.add_contact
+
+    # Return the new contact in the response
+    {
+      contact: contact.to_json_hash
+    }
+  end
+end # lambda_handler
