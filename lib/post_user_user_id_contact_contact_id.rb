7d2c9064afb52cdd087b8b804b5d06b76d556a80
Greg Meyers <greg.meyers.1138@gmail.com>
2025-07-12 13:55:14 -0700

    Add notes on testing a POST API

--- lib/post_user_user_id_contact_contact_id.rb
+++ lib/post_user_user_id_contact_contact_id.rb
@@ -2,6 +2,31 @@ require_relative 'jsc/require_all'
 require 'json'
 
 # POST /user/{user_id}/contact/{contact_id}
+#
+# Test from Lambda console:
+# {
+#   "pathParameters": {
+#     "user_id": "115610831205855378140",
+#     "contact_id": "c0000"
+#   },
+#   "queryStringParameters": {
+#     "access_token": "ya29.......0178"
+#   },
+#   "headers": {
+#     "origin": "http://localhost:3000"
+#   },
+#   "body": "{\"notes\": \"https://......./\"}"
+# }
+#
+# The access_token can be found in the Chrome developer console: first log into
+# the application, then hit F12, go to the Application tab and find the 'auth'
+# cookie and copy the value of "access_token"
+#
+# Test from curl:
+# curl -X POST -v -H "Origin: http://localhost:3000" \
+#   --json '{"notes": "http://www.yahoo.com/"}' \
+#   'https://jsc-tracker.infinitequack.net/user/115610831205855378140/contact/c0000?access_token=ya29....0178'
+#
 def lambda_handler(event:, context:)
   standard_json_handling(event: event) do |body:, access_token:|
     user_id = event.dig('pathParameters', 'user_id')
