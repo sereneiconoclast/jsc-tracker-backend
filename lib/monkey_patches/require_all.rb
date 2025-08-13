61b859a03975723bae20f4a9092a854180ba7432
Greg Meyers <greg.meyers.1138@gmail.com>
2025-08-12 17:11:52 -0700

    Drop unused email_to_sha1

--- lib/monkey_patches/require_all.rb
+++ lib/monkey_patches/require_all.rb
@@ -3,7 +3,6 @@ require_relative 'debug'
 
 require_relative 'db'
 require_relative 'easy_dig_hash_array'
-require_relative 'email_to_sha1'
 require_relative 'google_access_token'
 require_relative 'my_format'
 require_relative 'standard_json_handling'
