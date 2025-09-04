f839481398d81375426889de5d95cde87a8fd7eb
Greg Meyers <greg.meyers.1138@gmail.com>
2025-09-03 22:04:08 -0700

    Script creation of the jsc-tracker-lambda.yaml file
    This script is now capable of creating the existing CloudFormation template exactly

--- bin/build-cfn-templates.rb
+++ bin/build-cfn-templates.rb
@@ -4,16 +4,22 @@ require 'pathname'
 require 'active_support/core_ext/string/inflections' # camelize
 
 # Script to generate CloudFormation templates from Lambda handler metadata
+#
+# I considered using YAML (Psych) for this. Then you have to strip off the initial
+# document separator ("---"). Assembling the raw string seems easier, frankly.
 class CloudFormationTemplateBuilder
   def initialize
     @lib_dir = Pathname.new(__dir__).parent / 'lib'
   end
 
+  attr_accessor :debug
+  alias_method :debug?, :debug
+
   def operations
     @operations ||= begin
       handler_files.map do |file|
         parse_operation_metadata(file.read, file.basename.to_s)
-      end.compact.reduce(&:merge).tap { puts }
+      end.compact.reduce(&:merge).tap { say("\n") }
     end
   end
 
@@ -24,7 +30,8 @@ class CloudFormationTemplateBuilder
     function_name = "JSCTracker#{operation_name}"
     handler_name = "#{operation[:filename]}.lambda_handler"
 
-    <<~YAML
+    # Indented by 2 spaces
+    <<YAML
   #{function_name}LambdaFunction:
     Type: 'AWS::Lambda::Function'
     Properties:
@@ -41,18 +48,59 @@ class CloudFormationTemplateBuilder
 YAML
   end
 
+  def lambda_function_arn_stack_output(operation_name)
+    operation = operations[operation_name]
+    raise "Operation '#{operation_name}' not found" unless operation
+
+    function_name = "JSCTracker#{operation_name}"
+
+    # Indented by 2 spaces
+    <<YAML
+  #{function_name}LambdaFunctionArn:
+    Description: 'ARN of the #{operation_name} Lambda function'
+    Value: !GetAtt #{function_name}LambdaFunction.Arn
+    Export:
+      Name: !Sub '${AWS::StackName}-#{function_name}LambdaFunctionArn'
+YAML
+  end
+
+  def lambda_template
+    out = <<~YAML
+      AWSTemplateFormatVersion: '2010-09-09'
+      Description: 'CloudFormation template for JSCTracker Lambda functions'
+
+      Resources:
+    YAML
+    out << (
+      operations.keys.map(&method(:lambda_function)).join("\n")
+    )
+
+    out << "\nOutputs:\n"
+    out << (
+      operations.keys.map(&method(:lambda_function_arn_stack_output)).join('')
+    )
+
+    out
+  end
+
   private
+
+  def say(s)
+    puts(s) if debug?
+  end
 
   def handler_files
     @handler_files ||= begin
-      puts "Scanning for Lambda handler files in #{@lib_dir}..."
+      say "Scanning for Lambda handler files in #{@lib_dir}..."
 
       @lib_dir.glob('*.rb').select do |file|
         file.read.include?('def lambda_handler')
-      end.tap do |found|
-        puts "Found #{found.length} handler files:"
-        found.each { |file| puts "  - #{file.basename}" }
-        puts
+      end.sort.tap do |found|
+        if debug?
+          puts "Found #{found.length} handler files:"
+          found.each { |file| puts "  - #{file.basename}" }
+          puts
+        end
       end
     end
   end
@@ -62,7 +110,7 @@ YAML
   QUERY_PATTERN = %r!^Query:\s*(REQUIRED|OPTIONAL)\s+([a-z_]+)$!
 
   def parse_operation_metadata(content, filename)
-    puts "Parsing #{filename}..."
+    say "Parsing #{filename}..."
 
     # Extract the operation name from filename (remove .rb extension and CamelCase)
     filename = filename.gsub(/\.rb$/, '')
@@ -70,7 +118,7 @@ YAML
 
     # Find the =begin / =end block
     metadata_block = content.match(/=begin\nOPERATION METADATA:\n(.*?)\n=end/m)
-    return puts("  ✗ No operation metadata found") unless metadata_block
+    return say("  ✗ No operation metadata found") unless metadata_block
 
     metadata_text = metadata_block[1]
 
@@ -97,7 +145,7 @@ YAML
       end
     end
 
-    puts "  ✓ Parsed: #{operation_name} #{operation[:http_verb]} #{operation[:path]}"
+    say "  ✓ Parsed: #{operation_name} #{operation[:http_verb]} #{operation[:path]}"
 
     { operation_name => operation }
   end
@@ -106,10 +154,30 @@ end
 # Run the script
 if __FILE__ == $0
   builder = CloudFormationTemplateBuilder.new
-  operations = builder.operations
-  puts "Parsed Operations:"
-  puts "=" * 50
-  pp (operations)
-  puts
-  puts "Total operations: #{operations.size}"
+  case ARGV[0]
+  when 'operations'
+    builder.debug = true
+    raise "No argument expected" if ARGV[1]
+    operations = builder.operations
+    puts "Parsed Operations:"
+    puts "=" * 50
+    pp (operations)
+    puts
+    puts "Total operations: #{operations.size}"
+  when 'template'
+    case ARGV[1]
+    when 'lambda'
+      puts(builder.lambda_template)
+    else raise "Expected 'lambda'"
+    end
+  when 'operation'
+    case ARGV[1]
+    when 'lambda'
+      puts(builder.lambda_function(ARGV[2]))
+    when 'output'
+      puts(builder.lambda_function_arn_stack_output(ARGV[2]))
+    else raise "Expected 'lambda' or 'output'"
+    end
+  else raise "Expected 'operations', 'template', or 'operation'"
+  end
 end
